320f3d8d8d6a86e05b24ac6e535ed156b64a715b
Christian Seiler <christian@iwakd.de>
2015-08-22 09:58:36 +0200

    boot: wait long enough for multipath to create mappings
    When using multipath, the daemon will race for a lock against udev (and
    typically lose), and will wait for 1s before retrying that lock. Only
    then the multipath mappings will be added.
    
    The previous sleep of 0.1 seconds is too short to catch that
    regularily, so increase it to 3 seconds.
    
    On the other hand, if no volume groups should be activated directly
    (because LVM is not used - or the user uses lvmetad and has systemd),
    drop out early, because then it's unnecessary, because the boot will be
    entirely event-based.

--- debian/extra/activate-storage.sh
+++ debian/extra/activate-storage.sh
@@ -14,6 +14,21 @@ if [ -f /etc/default/open-iscsi ]; then
 	. /etc/default/open-iscsi
 fi
 
+# See if we need to handle LVM
+if [ ! -x $VGCHANGE ] && [ -n "$LVMGROUPS" ]; then
+	echo "Warning: LVM2 tools are not installed, not honouring LVMGROUPS." >&2
+	LVMGROUPS=""
+fi
+
+# If we don't have to activate any VGs and are running systemd, we
+# don't have to activate anything, so doing udevadm settle here and
+# potentially sleeping (if multipath is used) will not be productive,
+# because after waiting for both of these things, we will do nothing.
+# Therefore just drop out early if that is the case.
+if [ -d /run/systemd/system ] && [ -z "$LVMGROUPS" ] ; then
+	exit 0
+fi
+
 # Make sure we pick up all devices
 udevadm settle || true
 
@@ -23,16 +38,17 @@ if [ -x $MULTIPATH ] ; then
 	# twice to make sure multipathd has seen the devices and
 	# then been able to create the mappings.
 	# (We assume that multipathd is already running.)
-	sleep 0.1
+	#
+	# Note that multipathd will race against udev for locking the
+	# block device when it comes to creating the mappings, and it
+	# will retry only once per second (and will typically succeed
+	# on the second try), so we will wait three seconds here to be
+	# sure that it worked as expected.
+	sleep 3
 	udevadm settle || true
 fi
 
 # Handle LVM
-if [ ! -x $VGCHANGE ] && [ -n "$LVMGROUPS" ]; then
-	echo "Warning: LVM2 tools are not installed, not honouring LVMGROUPS." >&2
-	LVMGROUPS=""
-fi
-
 if [ -n "$LVMGROUPS" ] ; then
 	if ! $VGCHANGE -ay $LVMGROUPS ; then
 		echo "Warning: could not activate all LVM groups." >&2
